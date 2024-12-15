10188f170ae6eec99b8c1050d940ff5e00c87ed6
Nadjah Duke <nadjahduke@gmail.com>
2024-12-15 16:29:01 -0500

    Added validation to thymeleaf files and to TacoOrder and Taco class as well as th econtrolers

--- taco-cloud/src/main/java/com/tacocloud/taco_cloud/OrderController.java
+++ taco-cloud/src/main/java/com/tacocloud/taco_cloud/OrderController.java
@@ -1,6 +1,8 @@
 package com.tacocloud.taco_cloud;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus)
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus)
     {
+
+        if(errors.hasErrors())
+        {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
--- taco-cloud/src/main/java/com/tacocloud/taco_cloud/TacoOrder.java
+++ taco-cloud/src/main/java/com/tacocloud/taco_cloud/TacoOrder.java
@@ -1,5 +1,10 @@
 package com.tacocloud.taco_cloud;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import java.util.List;
 import java.util.ArrayList;
 import lombok.Data;
@@ -7,13 +12,28 @@ import lombok.Data;
 @Data
 public class TacoOrder
 {
+    @NotBlank(message = "Name is required")
     private String deliveryName;
+
+    @NotBlank(message = "Delivery Street is required")
     private String deliveryStreet;
+
+    @NotBlank(message = "Delivery City is required")
     private String deliveryCity;
+
+    @NotBlank(message = "Delivery State is required")
     private String deliveryState;
+
+    @Digits(integer = 5, fraction = 0, message = "Invalid Zipcode")
     private String deliveryZip;
+
+    @CreditCardNumber(message = "Not a Valid Credit Card Number")
     private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2]) ([\\/]) ([2-9][0-9])", message = "Must Be Formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
     private List <Taco> tacos = new ArrayList<>();
